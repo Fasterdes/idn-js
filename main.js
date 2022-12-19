b0c79346a77b8d980b46441587fa7a7b5c3b8d70
Caldera <keyn432des@gmail.com>
2022-12-19 05:09:15 +0000

    We are studying about javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+var name = "Fa Ainama Caldera Sudibyo"
+name = "Caldera"
+console.log(name)
+
+let age = 17 
+
+const result = 12.5
---
+++ type.js
@@ -0,0 +1,10 @@
+var name = "Fa Ainama Caldera S"
+
+// string is a data types of the characters.
+// variable name it will be store string data
+// string is assigned by ("") 
+
+var age = "17"
+
+// integer is a data types of number 
+// it does not need ("").. if you're assigned ("") it will be error. and he returned to string 
